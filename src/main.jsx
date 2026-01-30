8cd9d9a80b1d2de5949c286d4de1740dce9a923f
akani296m <akanimalu1@gmail.com>
2026-01-30 16:05:35 +0200

    Add Sentry initialization

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import * as Sentry from '@sentry/react'
 import App from './App.jsx'
 import './index.css'
 import { AuthProvider } from './context/authContext.jsx';
@@ -9,6 +10,13 @@ import { ProductProvider } from './context/productcontext.jsx';
 import { CollectionProvider } from './context/collectionContext.jsx';
 import { CartProvider } from './context/cartcontext.jsx';
 import { PostHogProvider } from 'posthog-js/react';
+
+Sentry.init({
+  dsn: 'https://00e68e309c8142206b60666f7beb7864@o4510799705669632.ingest.de.sentry.io/4510799726379088',
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
